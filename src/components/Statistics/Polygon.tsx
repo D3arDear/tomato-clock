26ae5f02d5d5b788aca6ca82517313660bc89814
BrendanZhang <ex.arc.zhang@gmail.com>
2020-04-13 17:20:42 +0800

    fix a bug of polygon

--- src/components/Statistics/Polygon.tsx
+++ src/components/Statistics/Polygon.tsx
@@ -7,16 +7,20 @@ interface PolygonProps {
 
 const Polygon: React.FunctionComponent<PolygonProps> = (props) => {
   const { data, width } = props;
+  const get0 = (time: string) => {
+    return new Date(new Date(time).toLocaleDateString()).getTime();
+  };
 
   const point = useMemo(() => {
     const dates = Object.keys(data).sort((a, b) => {
       return Date.parse(a) - Date.parse(b);
     });
+    const today = new Date(new Date().toLocaleDateString()).getTime();
     const verticalRange = dates.reduce((a, b) => (data[b].length > a ? data[b].length : a), 0);
-    const horizonRange = new Date().getTime() - Date.parse(dates[0]);
+    const horizonRange = today - get0(dates[0]);
     let lastHorizonPoint = 0;
     const points = dates.reduce((a, date) => {
-      const x = (1 - (new Date().getTime() - Date.parse(date)) / horizonRange) * width;
+      const x = (1 - (today - get0(date)) / horizonRange) * width;
       const y = (1 - data[date].length / verticalRange) * 60;
       lastHorizonPoint = x;
       return a.concat(` ${x},${y}`);
@@ -26,7 +30,7 @@ const Polygon: React.FunctionComponent<PolygonProps> = (props) => {
 
   return (
     <div className="Polygon" id="Polygon">
-      <svg width="100%" height="60" preserveAspectRatio="none">
+      <svg width={width} height="60" preserveAspectRatio="none">
         <polygon fill="rgba(255, 179, 113, 0.1)" stroke="rgba(255, 179, 113, 0.5)" strokeWidth="1" points={point} />
       </svg>
     </div>
