989ec02a704f8cd1cdfce475c75e946b5b073d51
Brendan <ex.arc.zhang@gmail.com>
2020-11-25 18:21:36 +0800

    adjust notification

--- src/components/Common/notification.ts
+++ src/components/Common/notification.ts
@@ -1,21 +1,17 @@
+const notificationBody = (ifFinishedTomato: boolean) => {
+  new Notification("番茄闹钟", {
+    lang: "zh-CN",
+    body: ifFinishedTomato ? "您已经完成了一个番茄时间" : "您已经休息了 5 分钟",
+  });
+};
 const createNotification = (ifFinishedTomato: boolean) => {
   if (!("Notification" in window)) {
   } else if (Notification.permission === "granted") {
-    new Notification(
-      ifFinishedTomato ? "您已经完成了一个番茄时间" : "您已经休息了 5 分钟",
-      {
-        lang: "zh-CN",
-      }
-    );
+    notificationBody(ifFinishedTomato);
   } else if (Notification.permission !== "denied") {
     Notification.requestPermission().then(function (permission) {
       if (permission === "granted") {
-        new Notification(
-          ifFinishedTomato ? "您已经完成了一个番茄时间" : "您已经休息了 5 分钟",
-          {
-            lang: "zh-CN",
-          }
-        );
+        notificationBody(ifFinishedTomato);
       }
     });
   }
