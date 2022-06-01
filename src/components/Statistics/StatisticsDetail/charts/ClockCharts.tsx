6d502c7eea8918789eb990cc29e8186276df3d84
Brendan <ex.arc.zhang@gmail.com>
2022-06-01 22:35:00 +0800

    update clock chart

--- src/components/Statistics/StatisticsDetail/charts/ClockCharts.tsx
+++ src/components/Statistics/StatisticsDetail/charts/ClockCharts.tsx
@@ -10,10 +10,30 @@ interface ClockChartsProps {
 const ClockCharts: React.FC<ClockChartsProps> = (props) => {
   const { width, bestMoment, timeData } = props;
 
+  const pieStroke = 5;
   const svgWidth = useMemo(() => (width > 720 ? width / 2 - 10 : width - 10), [width]);
   const circleR = useMemo(() => (svgWidth / 5 > 100 ? 100 : svgWidth / 5), [svgWidth]);
-  const pieStroke = 5;
+  const arcCircumference = useMemo(() => (circleR - pieStroke) * 2 * Math.PI, [circleR]);
+  const circleData = useMemo(() => {
+    const bestCount = timeData[bestMoment].count;
+    const startAng = (time: number) => (time > 11 ? time - 12 : time);
+    const endAng = (time: number) => (time > 11 ? time - 12 + 1 : time + 1);
+    return timeData.map((time: any) => {
+      return {
+        color: `rgba(255, 124, 54,${(bestCount === 0 ? 0 : time.count / bestCount) * 0.6})`,
+        startAng: startAng(parseInt(time.time)) * 30,
+        endAng: endAng(parseInt(time.time)) * 30,
+        dashedOffset: arcCircumference - (1 / 12) * arcCircumference,
+        style: {
+          transform: `rotate(${-120 + (startAng(parseInt(time.time)) + 1) * 30}deg)`,
+          transition: `0.35s stroke-dashoffset`,
+          transformOrigin: `${circleR}px ${circleR}px`,
+        },
+      };
+    });
+  }, [bestMoment, circleR, arcCircumference, timeData]);
   const pieData = useMemo(() => {
+    const pieStrokePrefix = 2.5 * pieStroke;
     const bestCount = timeData[bestMoment].count;
     const startAng = (time: number) => (time > 11 ? time - 12 : time);
     const endAng = (time: number) => (time > 11 ? time - 12 + 1 : time + 1);
@@ -23,19 +43,19 @@ const ClockCharts: React.FC<ClockChartsProps> = (props) => {
         endAng: endAng(parseInt(time.time)),
         x0:
           circleR +
-          (circleR - pieStroke) *
+          (circleR - pieStrokePrefix) *
             Math.cos(-Math.PI / 2 + startAng(parseInt(time.time)) * ((Math.PI * 2) / 12)),
         y0:
           circleR +
-          (circleR - pieStroke) *
+          (circleR - pieStrokePrefix) *
             Math.sin(-Math.PI / 2 + startAng(parseInt(time.time)) * ((Math.PI * 2) / 12)),
         x1:
           circleR +
-          (circleR - pieStroke) *
+          (circleR - pieStrokePrefix) *
             Math.cos(-Math.PI / 2 + endAng(parseInt(time.time)) * ((Math.PI * 2) / 12)),
         y1:
           circleR +
-          (circleR - pieStroke) *
+          (circleR - pieStrokePrefix) *
             Math.sin(-Math.PI / 2 + endAng(parseInt(time.time)) * ((Math.PI * 2) / 12)),
         color: `rgba(255, 124, 54,${(bestCount === 0 ? 0 : time.count / bestCount) * 0.6})`,
       };
@@ -47,27 +67,59 @@ const ClockCharts: React.FC<ClockChartsProps> = (props) => {
         <circle
           cx={circleR}
           cy={circleR}
-          r={circleR}
+          r={circleR - 2}
           fill="rgb(253,253,253)"
           stroke="#ddd"
-          strokeWidth="2"
+          strokeWidth="4"
         />
         <symbol id="tick">
-          <line x1={circleR} y1="0" x2={circleR} y2="5" stroke="#999" strokeWidth="1"></line>
+          <line
+            x1={circleR}
+            y1="0"
+            x2={circleR}
+            y2={pieStroke * 2}
+            stroke="#999"
+            strokeWidth="2"></line>
         </symbol>
+        <circle
+          cx={circleR}
+          cy={circleR}
+          r={circleR - pieStroke}
+          fill="none"
+          stroke="#aaaaaa13"
+          strokeWidth={`${pieStroke * 2}`}
+          strokeLinecap="round"
+          // strokeDashoffset={arcCircumference}
+        />
         {pieData.map((time: any, index: number) => (
           <path
             id={`${index}`}
             stroke="rgb(253,253,253)"
             strokeWidth={pieStroke}
+            strokeLinejoin="round"
             key={index}
             d={`M ${circleR} ${circleR},L ${time.x0} ${time.y0},A ${circleR},${circleR} 0 0,1 ${time.x1} ${time.y1},Z`}
             fill={`${time.color}`}
           />
         ))}
         {CircleTrick(circleR)}
-        {CircleNumber(circleR, bestMoment)}
-        <circle cx={circleR} cy={circleR} r={circleR * 0.5} fill="rgb(253,253,253)" />
+        {CircleNumber(circleR, bestMoment, pieStroke * 4)}
+        <circle cx={circleR} cy={circleR} r={circleR * 0.2} fill="rgb(253,253,253)" />
+        {circleData.map((arc: any, index: number) => (
+          <circle
+            key={index}
+            cx={circleR}
+            cy={circleR}
+            r={circleR - pieStroke}
+            fill="none"
+            stroke={arc.color}
+            strokeWidth={`${pieStroke * 2}`}
+            // strokeLinecap="round"
+            strokeDashoffset={arc.dashedOffset}
+            strokeDasharray={arcCircumference}
+            style={arc.style}
+          />
+        ))}
       </svg>
     </div>
   );
--- src/components/Statistics/StatisticsDetail/charts/ClockCommon.tsx
+++ src/components/Statistics/StatisticsDetail/charts/ClockCommon.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./ClockCharts.scss";
 
 const CircleTrick = (circleR: number) => {
-  return Object.keys(Array.from({ length: 4 })).map((item) => (
+  return Object.keys(Array.from({ length: 12 })).map((item) => (
     <use
       key={item}
       href="#tick"
-      transform={`rotate(${parseInt(item) * 90}, ${circleR}, ${circleR})`}
-    ></use>
+      transform={`rotate(${parseInt(item) * 30}, ${circleR}, ${circleR})`}></use>
   ));
 };
 
@@ -22,55 +21,28 @@ const ifActive = (bestMoment: number, item: string) =>
   bestMoment + 1 === parseInt(item) ||
   (bestMoment === 11 && parseInt(item) === 0);
 
-const CircleNumber = (circleR: number, bestMoment: string) =>
-  Object.keys(Array.from({ length: 12 })).map((item: string) =>
-    parseInt(item) > 9 || parseInt(item) === 0 ? (
-      <text
-        className={
-          ifActive(initBestMoment(bestMoment), item)
-            ? "circleNumber-best"
-            : "circleNumber"
-        }
-        key={item}
-        x={
-          circleR -
-          10 +
-          (circleR - 15) *
-            Math.cos(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
-        }
-        y={
-          circleR +
-          5 +
-          (circleR - 15) *
-            Math.sin(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
-        }
-      >
-        {parseInt(item) === 0 ? 12 : parseInt(item)}
-      </text>
-    ) : (
-      <text
-        className={
-          ifActive(initBestMoment(bestMoment), item)
-            ? "circleNumber-best"
-            : "circleNumber"
-        }
-        key={item}
-        x={
-          circleR -
-          5 +
-          (circleR - 15) *
-            Math.cos(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
-        }
-        y={
-          circleR +
-          5 +
-          (circleR - 15) *
-            Math.sin(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
-        }
-      >
-        {parseInt(item) === 0 ? 12 : parseInt(item)}
-      </text>
-    )
+const CircleNumber = (outerCircleR: number, bestMoment: string, offset: number) => {
+  const circleR = outerCircleR;
+  const doubleDigitOffsetX = useCallback(
+    (item: string) => (parseInt(item) > 9 || parseInt(item) === 0 ? 10 : 5),
+    []
   );
-
+  return Object.keys(Array.from({ length: 12 })).map((item: string) => (
+    <text
+      className={ifActive(initBestMoment(bestMoment), item) ? "circleNumber-best" : "circleNumber"}
+      key={item}
+      x={
+        circleR -
+        doubleDigitOffsetX(item) +
+        (circleR - offset) * Math.cos(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
+      }
+      y={
+        circleR +
+        5 +
+        (circleR - offset) * Math.sin(-Math.PI / 2 + parseInt(item) * ((Math.PI * 2) / 12))
+      }>
+      {parseInt(item) === 0 ? 12 : parseInt(item)}
+    </text>
+  ));
+};
 export { CircleTrick, CircleNumber };
